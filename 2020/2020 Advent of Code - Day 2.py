00d61aeacb67a751218c5cd5fb2c94bbb94c871b
Sean_or_Tony <anthonyasamoah48@gmail.com>
2022-02-10 08:28:44 +0000

    Finished without optimizations

--- 2020/2020 Advent of Code - Day 2.py
+++ 2020/2020 Advent of Code - Day 2.py
@@ -1,13 +1,18 @@
 from pprint import pprint
 from re import compile, findall
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.DEBUG)
+
+logging.debug('start of program\n')
 file = open('Day 2.txt', 'r')
 data = file.read()
 file.close()
+logging.info('File Loading Complete!\n')
 
 lines = data.split('\n')
 counter = 0
-
 
 for i in range(len(lines)):
 	lines[i] = lines[i].split()
@@ -16,24 +21,79 @@ for i in range(len(lines)):
 	minChar = int(charLen[0])
 	maxChar = int(charLen[1])
 
-	letter = lines[i][1]
+	Raw_letter = lines[i][1]
+	letter = str(Raw_letter[0])
 
 	pwd = lines[i][2]
 
-	found = 0
-	for j in pwd:
-		if j == letter:
-			found += 1
+	# Part 1
+	# ######################################
+	# found = 0
+	# Valid = False
+	# logging.debug("letter scan started")
+	# for j in pwd:
+	# 	if j == letter:
+	# 		found += 1
+	# 	else:
+	# 		pass
+	#
+	# if minChar <= found <= maxChar:
+	# 	Valid = True
+	# 	counter += 1
+	# else:
+	# 	pass
+	#
+	# logging.info(f"Found: {found}, {letter}, {minChar}:{pwd}:{maxChar}, Valid: {Valid}\n")
+    #
+	# ######################################
+
+	# Part 2
+	# ######################################
+	Valid = False
+	logging.debug("Letter scan initiated!")
+
+	check = False
+	checkmate = False
+
+	try:
+		if pwd[minChar - 1] == letter:
+			check = True
+			logging.debug(f'{pwd[minChar - 1] == letter} for {letter} == {pwd[minChar - 1]}')
+			if pwd[maxChar - 1] != letter:
+				check = True
+				logging.debug(f'{pwd[maxChar - 1] != letter} for {letter} != {pwd[maxChar - 1]}')
+				Valid = True
+				counter += 1
+			else:
+				logging.debug(f'{pwd[maxChar - 1] != letter} for {letter} != {pwd[maxChar - 1]}')
+				pass
 		else:
 			pass
 
-	if found > minChar or found < maxChar:
-		counter += 1
-	else:
+		if pwd[minChar - 1] != letter:
+			check = True
+			logging.debug(f'{pwd[minChar - 1] != letter} for {letter} != {pwd[minChar - 1]}')
+			if pwd[maxChar - 1] == letter:
+				check = True
+				logging.debug(f'{pwd[maxChar - 1] == letter} for {letter} == {pwd[maxChar - 1]}')
+				Valid = True
+				counter += 1
+			else:
+				logging.debug(f'{pwd[maxChar - 1] == letter} for {letter} == {pwd[maxChar - 1]}')
+				pass
+		else:
+			pass
+	except IndexError:
 		pass
 
-print(counter)
+	try:
+		logging.info(f" '{letter}', {minChar}:{pwd}:{maxChar}, Valid: {Valid}, Counter: {counter}\n")
+	except IndexError:
+		logging.info(
+			f" {letter}, {minChar}:{pwd}:{maxChar}, Valid: {Valid}, Counter: {counter}\n")
+	# ######################################
 
 
+print(f"\n#: {counter}")
 
-
+logging.debug('End of program')
